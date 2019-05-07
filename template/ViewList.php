6f0c7b55e5af234ce90f64720a9493e66d99ee16
unknown <nicovernot@gmail.com>
2019-05-07 20:34:46 +0200

    deux

--- template/ViewList.php
+++ template/ViewList.php
@@ -10,13 +10,23 @@ include "template/nav.php"
 
 <h1>List</h1>
 
-<?php
+<div class="container">
+<table style="width:100%" class="table-striped">
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th> 
+    <th>Mail</th>
+  </tr>
+  <?php
 foreach (SPDO::getInstance()->query('SELECT * FROM participant') as $membre)
 {
- echo  $membre[lastname];
+ echo "<tr><td>".$membre['firstname']."</td><td>".$membre['lastname']."</td><td>".$membre['mail']."</tr></td>";
  //echo '<pre>', print_r($membre) ,'</pre>';
 }
 ?>
+  
+</table>
+</div>
 
 
 <body>
